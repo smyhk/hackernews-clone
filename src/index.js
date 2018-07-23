04fd6abcf41f1d9517a6535d544137c86e5533af
Steve Kedzie <seal.team.smyhk@gmail.com>
2018-07-23 16:59:09 -0700

    Add Prisma bindings

--- src/index.js
+++ src/index.js
@@ -1,54 +1,39 @@
+const { Prisma } = require('prisma-binding');
 const { GraphQLServer } = require('graphql-yoga');
 
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}];
-
-let idCount = links.length;
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (root, args) => {
-      let link = links.find(l => {
-        return l.id === args.id;
-      });
-      return link;
-    }
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info);
+    },
   },
 
   Mutation: {
-    post: (root, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      }
-      links.push(link);
-      return link;
-    },
-    updateLink: (root, args) => {
-      let link = links.find(l => {
-        return l.id === args.id;
-      });
-      link['url'] = args.url;
-      link['description'] = args.description;
-      return link;
-    },
-    deleteLink: (root, args) => {
-      link = links.find(l => l.id === args.id);
-      links.pop(link);
-      console.info(links);
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink({
+        data: {
+          url: args.url,
+          description: args.description
+        }
+      }, info)
     }
   }
 };
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'http://localhost:4466',
+      secret: 'mysecret123',
+      debug: true
+    })
+  })
 });
 server.start(() => {
-  console.info(`Server is running on http://localhost:4000`);
+  console.info(`Server is running on http://localhost:4466`);
 });
